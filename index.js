068d387abc2e9bffc3cf421626005125051df73c
Anton Petrov <anton.a.petrov@gmail.com>
2021-05-23 12:23:57 +0300

    rnd button

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 
 const app = express();
-let port = process.env.PORT;
-console.log(port)
+let port = process.env.PORT; // для heroku
+
+// локальный запуск
 if (port == null || port == "") {
-    port = 3000;
+    port = 80;
 }
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('./public/index.html', { root: __dirname });
+    // res.send('<b>hello</b>')
 });
 
 app.get('/about', (req, res) => {
     res.sendFile('./public/about.html', { root: __dirname });
 });
 
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello, World!</h1>')
+});
+
+console.log('Веб-сервер запущен на порту ', port)
 app.listen(port);
--- public/script.js
+++ public/script.js
@@ -2,146 +2,202 @@ function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
 }
 
+function rgb2hex(rgb){
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? "#" +
+   ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+   ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+ }
+
+ function rgba2hex(orig) {
+  var a, isPercent,
+    rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+    alpha = (rgb && rgb[4] || "").trim(),
+    hex = rgb ?
+    (rgb[1] | 1 << 8).toString(16).slice(1) +
+    (rgb[2] | 1 << 8).toString(16).slice(1) +
+    (rgb[3] | 1 << 8).toString(16).slice(1) : orig;
+
+  if (alpha !== "") {
+    a = alpha;
+  } else {
+    a = 01;
+  }
+  // multiply before convert to HEX
+  a = ((a * 255) | 1 << 8).toString(16).slice(1)
+  hex = hex + a;
+
+  return '#' + hex.toString();
+}
+
 class Calculator {
+
+  isRound = false;
+
   constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
-    this.clear()
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
   }
 
   clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
   }
 
   delete() {
-    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
   appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
   chooseOperation(operation) {
-    if  (this.currentOperand === '') {
-      return
+    if (this.currentOperand === "") {
+      return;
     }
-    if (this.previousOperand !== '' || operation === 'sqrt') {
-      this.operation = operation
-      this.compute()
+    if (this.previousOperand !== "" || operation === "sqrt") {
+      this.operation = operation;
+      this.compute();
     }
-    if (operation !== 'sqrt') {
-      this.operation = operation
-      this.previousOperand = this.currentOperand
-      this.currentOperand = ''
-    }
-    else {
-      this.previousOperand = ''
+    if (operation !== "sqrt") {
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = "";
+    } else {
+      this.previousOperand = "";
     }
   }
 
   compute() {
-    let computation
-    const prev = parseFloat(this.previousOperand)
-    const current = parseFloat(this.currentOperand)
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
     // if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
-      case '+':
-        computation = prev + current
-        break
-      case '-':
-        computation = prev - current
-        break
-      case '*':
-        computation = prev * current
-        break
-      case '÷':
-        computation = prev / current
-        break
-      case 'log':
-        computation = getBaseLog(prev, current)
-        break
-      case 'x^y':
-        computation = Math.pow(prev, current)
-        break
-      case 'sqrt':
-        computation = Math.sqrt(current)
-        break
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      case "log":
+        computation = getBaseLog(prev, current);
+        break;
+      case "sqrt":
+        computation = Math.sqrt(current);
+        break;
       default:
-        return
+        return;
     }
-    this.currentOperand = computation
-    this.operation = undefined
-    this.previousOperand = ''
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = "";
   }
 
   getDisplayNumber(number) {
-    const stringNumber = number.toString()
-    const integerDigits = parseFloat(stringNumber.split('.')[0])
-    const decimalDigits = stringNumber.split('.')[1]
-    let integerDisplay
+    if(this.isRound) {
+      number = Math.round(number * 1000) / 1000
+    }
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
     if (isNaN(integerDigits)) {
-      integerDisplay = ''
+      integerDisplay = "";
     } else {
-      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      integerDisplay = integerDigits.toLocaleString("ru", {
+        maximumFractionDigits: 0,
+        useGrouping: 'true'
+        //style: 'currency',
+        //currency: 'RUB',
+      });
     }
     if (decimalDigits != null) {
-      return `${integerDisplay}.${decimalDigits}`
+      return `${integerDisplay}.${decimalDigits}`;
     } else {
-      return integerDisplay
+      return integerDisplay;
     }
   }
 
   updateDisplay() {
-    this.currentOperandTextElement.innerText =
-      this.getDisplayNumber(this.currentOperand)
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(
+      this.currentOperand
+    );
     if (this.operation != null) {
-      this.previousOperandTextElement.innerText =
-        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
     } else {
-      this.previousOperandTextElement.innerText = ''
+      this.previousOperandTextElement.innerText = "";
     }
   }
 }
 
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const rndButton = document.querySelector("[data-rnd]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
 
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateDisplay()
-  })
-})
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-operationButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    calculator.chooseOperation(button.innerText)
-    calculator.updateDisplay()
-  })
-})
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
 
-equalsButton.addEventListener('click', button => {
-  calculator.compute()
-  calculator.updateDisplay()
-})
+rndButton.addEventListener("click", (button) => {
+  calculator.isRound = !calculator.isRound;
+  if(calculator.isRound) {
+    console.log(rgba2hex('rgba(180, 180, 180, 0.75)'))
+    rndButton.style.backgroundColor = rgba2hex('rgba(180, 180, 180, 0.75)');
+  }
+  else {
+    console.log(rgba2hex('rgba(255, 255, 255, 0.75)'))
+    rndButton.style.backgroundColor = rgba2hex('rgba(255, 255, 255, 0.75)');
+  }
+  calculator.updateDisplay();
+});
 
-allClearButton.addEventListener('click', button => {
-  calculator.clear()
-  calculator.updateDisplay()
-})
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
-deleteButton.addEventListener('click', button => {
-  calculator.delete()
-  calculator.updateDisplay()
-})
+deleteButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
